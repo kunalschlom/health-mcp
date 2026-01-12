d8bc17caf7f87f7d90069b7985e7d2bd136639f1
Kunal <fergyfaltu@gmail.com>
2026-01-13 01:59:48 +0530

    c

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import os
 
 def create_model():
     
-   hf_token=os.getenv("HF_TOKEN")
+   hf_token="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"
    if not hf_token:
         raise ValueError("the hf token is not available")   
    repo_id="Qwen/Qwen2.5-7B-Instruct"     
